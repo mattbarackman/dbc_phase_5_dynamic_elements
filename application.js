fe0ad072cd8e297d95c59b3a3707dd48dcc23c94
mattbarackman <matthew.barackman@gmail.com>
2013-06-13 16:33:24 -0700

    Added js for addition, removal, and change status on todo items

--- application.js
+++ application.js
@@ -1,14 +1,36 @@
 $(document).ready(function() {
   var todoTemplate = $.trim($('#todo_template').html());
+  // console.log(todoTemplate);
 
   function bindEvents() {
     // Bind functions which add, remove, and complete todos to the appropriate
     // elements
+    
+    // Add Todo
+    $('.toolbox').on('click', '.add', function(e){
+      e.preventDefault();
+      var todo_text = $('.todo').val();
+      var builtTodo = buildTodo(todo_text);
+      addTodo(builtTodo);
+    });
+
+    // Remove Todo
+    $('.todo_list').on('click', '.delete', function(e){
+      e.preventDefault();
+      var todo = $(this).closest('.todo');
+      removeTodo(todo);
+    });
+
+    $('.todo_list').on('click', '.complete', function(e){
+      e.preventDefault();
+      var todo = $(this).closest('.todo');
+      if (todo.hasClass('completed')) {
+        markTodoIncomplete(todo);
+      } else {markTodoComplete(todo);}
+    });
   }
 
   //Create functions to add, remove and complete todos
-
-  
 
   function buildTodo(todoName) {
     // Creates an jQueryDOMElement from the todoTemplate.
@@ -19,6 +41,23 @@ $(document).ready(function() {
     return $todo;
   }
   
+  function addTodo(todo) {
+    $('.todo_list').prepend(todo);
+  }
+
+  function removeTodo(todo) {
+    $(todo).fadeOut(500, function() { $(todo).remove(); });
+  }
+
+  function markTodoComplete(todo) {
+    $(todo).addClass('completed');
+    $(todo).find('.complete').html("Uncomplete");
+  }
+
+  function markTodoIncomplete(todo) {
+    $(todo).removeClass('completed');
+    $(todo).find('.complete').html("Complete");
+  }
 
   bindEvents();
 });

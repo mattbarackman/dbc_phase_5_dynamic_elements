a57948f5989895458d28a95d101f81be48dc79a4
mattbarackman <matthew.barackman@gmail.com>
2013-06-18 20:39:18 -0700

    moved todoTemplate in scope of buildtodo

--- application.js
+++ application.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
 
-  var todoTemplate = $.trim($('#todo_template').html());
-
   function bindEvents() {
     // Bind functions which add, remove, and complete todos to the appropriate
     // elements
@@ -33,6 +31,7 @@ $(document).ready(function() {
   //Create functions to add, remove and complete todos
 
   function buildTodo(todoName) {
+    var todoTemplate = $.trim($('#todo_template').html());
     // Creates an jQueryDOMElement from the todoTemplate.
     var $todo = $(todoTemplate);
     // Modifies it's text to use the passed in todoName.
